7dec3423f088a7a9aa0404c013313af1a3d867b4
Rillke <rillke@wikipedia.de>
2014-07-21 19:18:20 +0200

    Adding recorder base class and HTML5 implementation
    ... and the library

---
+++ prg.html5recorder.js
@@ -0,0 +1,187 @@
+/*! Copyright 2014 Rainer Rillke <lastname@wikipedia.de>
+ * Quadrouple-licensed under
+ * - GPLv2 and, at your option any later version of that license,
+ * - LGPL 2.1 and, at your option any later version of that license,
+ * - Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0),
+ * - GNU Free Documentation License (GNU FDL aka. GFDL) 1.2
+ *   and, at your option any later version of that license.
+ *
+ * @author Rainer Rillke
+ */
+
+/**
+ * Pronunciation recording gadget.
+ * Framework for recording audio in a browser.
+ *
+ * Funded by the Wikimedia Foundation as part of an Individual Investment Grant.
+ * This means the Wikimedia Foundation supported creation materially
+ * but did not directly endorse creation of this software.
+ * More information about
+ * [Individual Investment Grants](https://meta.wikimedia.org/wiki/Grants:IEG).
+ *
+ * @class prg.Html5Recorder
+ * Recording using HTML5's APIs.
+ *
+ * @inheritdoc prg.Recorder
+ * @requires jQuery
+ */
+
+
+( function( $, global ) {
+
+
+	// Make sure the namespace exists
+	if ( !global.prg ) {
+		throw new Error( "Base class and namespace required here!" );
+	}
+
+	// Normalize several vendor-specific methods
+	try {
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		navigator.getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia );
+		window.URL = window.URL || window.webkitURL;
+	} catch ( writeProtected ) {}
+
+	global.prg.Html5Recorder = function() {};
+	$.extend( global.prg.Html5Recorder.prototype, {
+		isCompatible: function() {
+			var $def = $.Deferred(),
+				compat = !!( ( window.URL ) && ( window.AudioContext ) && ( navigator.getUserMedia ) && ( window.Blob && window.File ) );
+			if ( compat ) {
+				$def.resolve();
+			} else {
+				$def.reject();
+			}
+			return $def.promise();
+		},
+
+		hasMicrophoneAccess: false,
+
+		requestMicrophoneAccess: function() {
+			var $def = $.Deferred(),
+				html5Recorder = this;
+
+			try {
+				html5Recorder.audioContext = new AudioContext();
+			} catch ( e ) {
+				// This should never happen but better safe than sorry
+				mw.log( 'WebAudio API is not properly supported for this browser' );
+				throw e;
+			}
+			navigator.getUserMedia( {
+				audio: true
+			}, function( stream ) {
+				html5Recorder.stream = stream;
+
+				$def.resolve();
+			}, function() {
+				$def.reject();
+			} );
+			return $def.promise();
+		},
+
+		getRecording: function() {
+			var $def = $.Deferred();
+			if ( !this.hasMicrophoneAccess ) {
+				throw new Error( 'Request access to the microphone before attempting to record!' );
+			}
+			return new global.prg.Recording( this );
+		}
+	} );
+
+	/**
+	 * @class prg.Html5Recording
+	 * Represents a recording.
+	 *
+	 * @inheritdoc prg.Html5Recording
+	 * @requires jQuery
+	 */
+	global.prg.Html5Recording = function( html5Recorder ) {
+		this.html5Recorder = html5Recorder;
+	};
+	$.extend( global.prg.Html5Recording.prototype, {
+
+		start: function() {
+			var $def = $.Deferred();
+
+			if ( !this.html5Recorder.input ) {
+				// audio playback from the MediaStream will be re-routed into the processing graph of the AudioContext
+				this.html5Recorder.input = this.html5Recorder.audioContext.createMediaStreamSource( stream );
+			}
+			recorder = this.recorder = new Recorder( this.html5Recorder.input );
+			this.waveBlob = null;
+			recorder.clear();
+			recorder.record();
+			$def.resolve();
+			return $def.promise();
+		},
+
+		stopRecording: function() {
+			var $def = $.Deferred();
+
+			this.recorder.stop();
+			$def.resolve();
+			return $def.promise();
+		},
+
+		getPlayer: function() {
+			var player, $def = $.Deferred();
+
+			if ( !this.waveBlob ) return $def.reject()
+				.promise();
+
+			player = $.parseHTML( '<audio controls class="prg-preview-audio"><source src="' + URL.createObjectURL( this.waveBlob ) + '" type="audio/wav"></audio>' );
+			$def.resolve( player );
+			return $def.promise();
+		},
+
+		play: function() {
+			var $def = $.Deferred(),
+				audioElem;
+
+			if ( !this.waveBlob ) return $def.reject()
+				.promise();
+
+			audioElem = document.createElement( 'audio' );
+
+			player.on( 'load', function() {
+				audioElem.play();
+			}, true );
+			audioElem.setAttribute( 'src', URL.createObjectURL( this.waveBlob ) );
+			audioElem.play();
+			$def.resolve( audioElem );
+			return $def.promise();
+		},
+
+		stopPlay: function( player ) {
+			var $def = $.Deferred();
+			if ( !player ) return $def.reject()
+				.promise();
+
+			$def.resolve();
+			return $def.promise();
+		},
+
+		/**
+		 * Not implemented
+		 * @method upload
+		 */
+		upload: null,
+
+		getData: function() {
+			var $def = $.Deferred();
+			if ( this.waveBlob ) {
+				$def.resolve( this.waveBlob );
+			} else {
+				this.recorder.exportWAV(
+					function( blob ) {
+						this.waveBlob = blob;
+						$def.resolve( blob );
+					}
+				);
+			}
+			return $def.promise();
+		}
+	} );
+
+} )( jQuery, window.mediaWiki ? mediaWiki.libs : window );
---
+++ prg.recorder.js
@@ -0,0 +1,214 @@
+/*! Copyright 2014 Rainer Rillke <lastname@wikipedia.de>
+ * Quadrouple-licensed under
+ * - GPLv2 and, at your option any later version of that license,
+ * - LGPL 2.1 and, at your option any later version of that license,
+ * - Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0),
+ * - GNU Free Documentation License (GNU FDL aka. GFDL) 1.2
+ *   and, at your option any later version of that license.
+ *
+ * @author Rainer Rillke
+ */
+
+/**
+ * Pronunciation recording gadget.
+ * Framework for recording audio in a browser.
+ *
+ * Funded by the Wikimedia Foundation as part of an Individual Investment Grant.
+ * This means the Wikimedia Foundation supported creation materially
+ * but did not directly endorse creation of this software.
+ * More information about
+ * [Individual Investment Grants](https://meta.wikimedia.org/wiki/Grants:IEG).
+ *
+ * @class prg.Recorder
+ * Base recorder class.
+ * Each recording approach must implement prg.Recorder
+ * and prg.Recording and those must be shipped together.
+ *
+ * @abstract
+ * @requires jQuery
+ */
+
+
+( function( $, global ) {
+
+	/**
+	 * Object.create polyfill
+	 * source:
+	 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create#Polyfill
+	 */
+	if ( typeof Object.create !== 'function' ) {
+		( function() {
+			var F = function() {};
+			Object.create = function( o ) {
+				if ( arguments.length > 1 ) {
+					throw Error( 'Second argument not supported' );
+				}
+				if ( o === null ) {
+					throw Error( 'Cannot set a null [[Prototype]]' );
+				}
+				if ( typeof o !== 'object' ) {
+					throw TypeError( 'Argument must be an object' );
+				}
+				F.prototype = o;
+				return new F();
+			};
+		} )();
+	}
+
+
+	// Make sure the namespace exists
+	if ( !global.prg ) {
+		global.prg = {};
+	}
+
+	global.prg.Recorder = function() {};
+	$.extend( global.prg.Recorder.prototype, {
+		/**
+		 * Compatiblity check for the recorder type on the currently chosen client
+		 *
+		 * @abstract
+		 * @static
+		 * @return {jQuery.Promise}
+		 */
+		isCompatible: function() {
+			var $def = $.Deferred();
+			$def.reject();
+			return $def.promise();
+		},
+
+		hasMicrophoneAccess: false,
+
+		/**
+		 * Requests access to the microphone.
+		 * This may open a confirmation dialog.
+		 * Must set .hasMicrophoneAccess to true when
+		 * successful and before resolving the Deferred.
+		 *
+		 * @abstract
+		 * @return {jQuery.Promise}
+		 */
+		requestMicrophoneAccess: function() {
+			var $def = $.Deferred();
+			$def.reject();
+			return $def.promise();
+		},
+
+		/**
+		 * Create a recording and return a reference to
+		 * that recording.
+		 *
+		 * @abstract
+		 * @return {prg.Recording}
+		 */
+		getRecording: function() {
+			var $def = $.Deferred();
+			if ( !this.hasMicrophoneAccess ) {
+				throw new Error( 'Request access to the microphone before attempting to record!' );
+			}
+			return new global.prg.Recording();
+		}
+	} );
+
+	/**
+	 * @class prg.Recording
+	 * Base recording class.
+	 * Represents a recording.
+	 *
+	 * @abstract
+	 * @requires jQuery
+	 */
+	global.prg.Recording = function() {};
+	$.extend( global.prg.Recording.prototype, {
+		/**
+		 * Starts recording.
+		 *
+		 * @abstract
+		 * @return {jQuery.Promise}
+		 */
+		start: function() {
+			var $def = $.Deferred();
+			$def.reject();
+			return $def.promise();
+		},
+
+		/**
+		 * Stops recording.
+		 *
+		 * @abstract
+		 * @return {jQuery.Deferred}
+		 */
+		stopRecording: function() {
+			var $def = $.Deferred();
+			$def.reject();
+			return $def.promise();
+		},
+
+		/**
+		 * Get a player element.
+		 * First and only argument to the .done() callback
+		 * of the Deferred is the HTMLElement containing the player.
+		 *
+		 * @abstract
+		 * @return {jQuery.Promise}
+		 */
+		getPlayer: function() {
+			var $def = $.Deferred();
+			$def.reject();
+			return $def.promise();
+		},
+
+		/**
+		 * Plays the previously recorded sound.
+		 *
+		 * @abstract
+		 * @return {jQuery.Promise}
+		 */
+		play: function() {
+			var $def = $.Deferred();
+			$def.reject();
+			return $def.promise();
+		},
+
+		/**
+		 * Stops playing the previously recorded sound.
+		 *
+		 * @abstract
+		 * @param {Object} player obtained through `.play()`
+		 * @return {jQuery.Promise}
+		 */
+		stopPlay: function( player ) {
+			var $def = $.Deferred();
+			$def.reject();
+			return $def.promise();
+		},
+
+		/**
+		 * Uploads the previously recorded sound.
+		 * Note derived classes can either implement
+		 * `.upload()` or `.getData()` or both.
+		 *
+		 * @abstract
+		 * @method upload
+		 * @return {jQuery.Promise}
+		 */
+		upload: null,
+
+		/**
+		 * Get the data of the previously recorded sound.
+		 * Note derived classes can either implement
+		 * `.upload()` or `.getData()` or both.
+		 * First and only argument to the .done() callback
+		 * of the Deferred is an object of the structure
+		 *     {
+		 *         data: Blob,
+		 *         type: 'MIME type'
+		 *     }
+		 *
+		 * @abstract
+		 * @method getData
+		 * @return {jQuery.Promise}
+		 */
+		getData: null
+	} );
+
+} )( jQuery, window.mediaWiki ? mediaWiki.libs : window );
